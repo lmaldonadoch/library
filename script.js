e19351c09b8259ecffb5852318dee2b7f38d6b25
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-06-10 15:42:37 -0500

    Fixed render function to create elements with different id

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 let myLibrary = ["The Lord of the Rings", "HP", "Narnia"];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
 }
 
 function addBookToLibrary() {
   // do stuff here
-  var addBook = document.getElementById("AddBook");
+  var addBook = document.getElementById("addbook");
   if (addBook.style.display == "none") {
-    addBook.style.display == "flex";
-    "<p> TEST </p>"
+    addBook.style.display = "flex";
   } else {
-    addBook.style.display == "none";
+    addBook.style.display = "none";
   }
 }
 
-var render = function (template, node) {
-  node.innerHTML = template.join(" ");
+var render = function (arr, node) {
+  arr.forEach((book, index) => {
+    let div = document.createElement("DIV");
+    let para = document.createElement("P");
+    let btn = document.createElement("BUTTON");
+    para.innerHTML = book;
+    btn.innerHTML = "Delete";
+    div.setAttribute("id", `book-${index}`);
+    div.appendChild(para);
+    div.appendChild(btn);
+    btn.setAttribute("value", `book-${index}`);
+    document.getElementById("main").appendChild(div);
+  });
 };
 
-var template = [];
-
-myLibrary.forEach((book) => {
-  template.push(`<p>${book}</p>`);
-});
-
-render(template, document.querySelector("#main"));
+render(myLibrary, document.querySelector("#main"));
