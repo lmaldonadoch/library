549667de89247d3d206ec3a11387155efa573e49
idgm5 <idgm@pm.me>
2020-06-11 11:39:34 -0500

    WIP delete func for book

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ var save = function (array) {
 };
 
 var deleteBookFromLibrary = function (book) {
-  delete myLibrary[book];
+  delete myLibrary[book.target.attributes.data.value];
   myLibrary = myLibrary.filter((elem) => {
     elem != null;
   });
@@ -47,11 +47,24 @@ var deleteBookFromLibrary = function (book) {
   save(myLibrary);
 };
 
+var changeReadStatus = function(book) {
+  console.log(myLibrary);
+  console.log(book);
+
+  if(myLibrary[book].read == true) {
+    myLibrary[book].read = false;
+  } else {
+    myLibrary[book].read = true;
+  }
+    save(myLibrary);
+}
+
 var render = function (arr, node) {
   arr.forEach((book, index) => {
     let div = document.createElement("DIV");
     let para = document.createElement("P");
     let btn = document.createElement("BUTTON");
+    let btnRead = document.createElement("BUTTON");
     para.innerHTML = `${book.title} was written by ${book.author}
     and contains ${book.pages} pages.`;
     if (book.read == true) {
@@ -60,14 +73,19 @@ var render = function (arr, node) {
       para.innerHTML = para.innerHTML + " You haven't read this book.";
     }
     btn.innerHTML = "Delete";
+    btnRead.innerHTML = "Read";
     div.setAttribute("id", `book-${index}`);
     div.setAttribute("class", "books");
     div.appendChild(para);
     div.appendChild(btn);
+    div.appendChild(btnRead);
     btn.setAttribute("id", "delete");
     btn.setAttribute("data", `${index}`);
     btn.setAttribute("class", "books-button");
-    // btn.addEventListener("click", deleteBookFromLibrary(div));
+    btn.setAttribute("type", "submit");
+    btnRead.setAttribute("id", "read");
+    btnRead.setAttribute("class", "read-button");
+    btnRead.setAttribute("type", "submit");
     document.getElementById("main").appendChild(div);
   });
 };
@@ -75,10 +93,16 @@ var render = function (arr, node) {
 render(myLibrary, document.querySelector("#main"));
 
 const buttons = document.getElementsByClassName("books-button");
+const buttonsRead = document.getElementsByClassName("read-button");
 
-console.log(buttons);
+console.log(myLibrary);
 
 [...buttons].forEach((button) => {
   console.log(button);
   button.addEventListener("click", deleteBookFromLibrary);
+});
+
+[...buttonsRead].forEach((button) => {
+  console.log(button);
+  button.addEventListener("click", changeReadStatus);
 });
